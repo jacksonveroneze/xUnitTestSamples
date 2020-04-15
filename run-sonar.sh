619aeb01b482d50d97a95595a35e4c6fc4875ad2
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-04-15 14:07:37 -0300

    TDD.

--- run-sonar.sh
+++ run-sonar.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 rm -rf .sonarqube
-rm 01-Unit-Tests/xUnitTestSamples.Features.Tests/coverage.opencover.xml
-rm 01-Unit-Tests/xUnitTestSamples.Basic.Tests/coverage.opencover.xml
+find . -name '*.opencover.xml' -exec rm {} \;
 
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
+FILES=$(find . -type f -name "*.opencover.xml")
 
 dotnet sonarscanner begin \
 /d:sonar.host.url="https://sonarcloud.io" \
 /o:jacksonveroneze \
 /d:sonar.login=d7f50518d0cc81c1ef1bc03a04031be58ae71e3d \
 /k:xUnitTestSamples \
-/d:sonar.cs.opencover.reportsPaths="01-Unit-Tests/xUnitTestSamples.Features.Tests/coverage.opencover.xml, 01-Unit-Tests/xUnitTestSamples.Basic.Tests/coverage.opencover.xml, 02-TDD\tests/NerdStore.Vendas.Domain.Tests/coverage.opencover.xml" \
-/d:sonar.exclusions="01-Unit-Tests/xUnitTestSamples.Features.Tests/**, 01-Unit-Tests/UnitTestSamples.Features.Tests/**, 02-TDD\tests/NerdStore.Vendas.Domain.Tests/**" \
-/d:sonar.coverage.exclusions="01-Unit-Tests/xUnitTestSamples.Features.Tests/**, 01-Unit-Tests/UnitTestSamples.Features.Tests/**, 02-TDD\tests/NerdStore.Vendas.Domain.Tests/**" \
+/d:sonar.cs.opencover.reportsPaths="01-Unit-Tests/xUnitTestSamples.Features.Tests/coverage.opencover.xml, 01-Unit-Tests/xUnitTestSamples.Basic.Tests/coverage.opencover.xml, 02-TDD/tests/NerdStore.Vendas.Domain.Tests/coverage.opencover.xml" \
+/d:sonar.exclusions="01-Unit-Tests/xUnitTestSamples.Features.Tests/**, 01-Unit-Tests/UnitTestSamples.Features.Tests/**, 02-TDD/tests/NerdStore.Vendas.Application.Tests/**, 02-TDD/tests/NerdStore.Vendas.Domain.Tests/**" \
+/d:sonar.coverage.exclusions="01-Unit-Tests/xUnitTestSamples.Features.Tests/**, 01-Unit-Tests/UnitTestSamples.Features.Tests/**, 02-TDD/tests/NerdStore.Vendas.Application.Tests/**, 02-TDD/tests/NerdStore.Vendas.Domain.Tests/**" \
 /v:1.0.0
 
 dotnet build
