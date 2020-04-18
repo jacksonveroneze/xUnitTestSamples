86e5fbe1c7823b64f07171e6f3bacf699166ae94
Jackson <jackson@jacksonveroneze.com>
2020-04-18 20:20:50 -0300

    Testes de integração

---
+++ 03-Integration-Tests/src/NerdStore.WebApp.MVC/Models/ItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NerdStore.WebApp.MVC.Models
+{
+    public class ItemViewModel
+    {
+        public Guid Id { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
---
+++ 03-Integration-Tests/src/NerdStore.WebApp.MVC/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NerdStore.WebApp.MVC.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
