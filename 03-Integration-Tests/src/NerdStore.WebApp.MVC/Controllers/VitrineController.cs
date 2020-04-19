091d6bb1a8b98c0db71b662344187d6e8235f90c
Jackson <jackson@jacksonveroneze.com>
2020-04-19 18:33:10 -0300

    Testes de integração

--- 03-Integration-Tests/src/NerdStore.WebApp.MVC/Controllers/VitrineController.cs
+++ 03-Integration-Tests/src/NerdStore.WebApp.MVC/Controllers/VitrineController.cs
@@ -1,10 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NerdStore.Catalogo.Application.Services;
 
 namespace NerdStore.WebApp.MVC.Controllers
 {
-    class VitrineController
+    public class VitrineController : Controller
     {
+        private readonly IProdutoAppService _produtoAppService;
+
+        public VitrineController(IProdutoAppService produtoAppService)
+        {
+            _produtoAppService = produtoAppService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _produtoAppService.ObterTodos());
+        }
+
+        [HttpGet]
+        [Route("produto-detalhe/{id}")]
+        public async Task<IActionResult> ProdutoDetalhe(Guid id)
+        {
+            return View(await _produtoAppService.ObterPorId(id));
+        }
     }
 }
--- 03-Integration-Tests/tests/NerdStore.WebApp.Tests/PedidoWebTests.cs
+++ 03-Integration-Tests/tests/NerdStore.WebApp.Tests/PedidoWebTests.cs
@@ -1,14 +1,66 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
 using Xunit;
 
 namespace NerdStore.WebApp.Tests
 {
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class PedidoWebTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly IntegrationTestsFixture<StartupWebTests> _testsFixture;
 
+        public PedidoWebTests(IntegrationTestsFixture<StartupWebTests> testsFixture)
+            => _testsFixture = testsFixture;
+
+        [Fact(DisplayName = "Adicionar item em novo pedido")]
+        [Trait("Categoria", "Integração Web - Pedido")]
+        public async Task AdicionarItem_NovoPedido_DeveAtualizarValorTotal()
+        {
+            // Arrange
+            Guid produtoId = new Guid("191ddd3e-acd4-4c3b-ae74-8e473993c5da");
+
+            const int quantidade = 2;
+
+            HttpResponseMessage initialResponse = await _testsFixture.Client.GetAsync($"/produto-detalhe/{produtoId}");
+
+            initialResponse.EnsureSuccessStatusCode();
+
+            IDictionary<string, string> formData = new Dictionary<string, string>
+            {
+                {"Id", produtoId.ToString()},
+                {"quantidade", quantidade.ToString()}
+            };
+
+            await _testsFixture.RealizarLoginWeb();
+
+            HttpRequestMessage postRequest = new HttpRequestMessage(HttpMethod.Post, "/meu-carrinho")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+            // Act
+            HttpResponseMessage postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+            // Assert
+            postResponse.EnsureSuccessStatusCode();
+
+            IHtmlAllCollection html = new HtmlParser()
+                .ParseDocumentAsync(await postResponse.Content.ReadAsStringAsync())
+                .Result
+                .All;
+
+            decimal? formQuantidade = html?.FirstOrDefault(c => c.Id == "quantidade")?.GetAttribute("value")?.ApenasNumeros();
+            decimal? valorUnitario = html?.FirstOrDefault(c => c.Id == "valorUnitario")?.TextContent.Split(".")[0]?.ApenasNumeros();
+            decimal? valorTotal = html?.FirstOrDefault(c => c.Id == "valorTotal")?.TextContent.Split(".")[0]?.ApenasNumeros();
+
+            Assert.Equal(valorTotal, valorUnitario * formQuantidade);
         }
     }
 }
