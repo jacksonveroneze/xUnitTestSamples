931e49f61b63b97a9f3570236f6020f89ba544b5
Jackson <jackson@jacksonveroneze.com>
2020-04-18 21:26:59 -0300

    Testes de integração

--- 03-Integration-Tests/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
+++ 03-Integration-Tests/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -1,14 +1,55 @@
-using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
 using Xunit;
 
 namespace NerdStore.WebApp.Tests
 {
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class UsuarioTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly IntegrationTestsFixture<StartupWebTests> _testsFixture;
 
+        public UsuarioTests(IntegrationTestsFixture<StartupWebTests> testsFixture)
+            => _testsFixture = testsFixture;
+
+        [Fact(DisplayName = "Realizar cadastro com sucesso")]
+        [Trait("Categoria", "Integração Web - Usuário")]
+        public async Task Usuario_RealizarCadastro_DeveExecutarComSucesso()
+        {
+            // Arrange
+            HttpResponseMessage initialResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
+            initialResponse.EnsureSuccessStatusCode();
+
+            var antiForgeryToken =
+                _testsFixture.ObterAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
+
+            _testsFixture.GerarUserSenha();
+
+            var formData = new Dictionary<string, string>
+            {
+                { _testsFixture.AntiForgeryFieldName, antiForgeryToken },
+                {"Input.Email", _testsFixture.UsuarioEmail },
+                {"Input.Password", _testsFixture.UsuarioSenha },
+                {"Input.ConfirmPassword", _testsFixture.UsuarioSenha }
+            };
+
+            HttpRequestMessage postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+            // Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+            // Assert
+            string responseString = await postResponse.Content.ReadAsStringAsync();
+
+            postResponse.EnsureSuccessStatusCode();
+
+            Assert.Contains($"Hello {_testsFixture.UsuarioEmail}", responseString);
         }
     }
 }
